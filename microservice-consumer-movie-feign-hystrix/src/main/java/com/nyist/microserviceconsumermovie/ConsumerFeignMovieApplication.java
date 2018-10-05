2eedd9b098992189bc8861cbe0421a89bc71ce34
buchuang <bu94450@163.com>
2018-10-05 17:06:23 +0800

    dashboard仪表板

--- microservice-consumer-movie-feign-hystrix/src/main/java/com/nyist/microserviceconsumermovie/ConsumerFeignMovieApplication.java
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/nyist/microserviceconsumermovie/ConsumerFeignMovieApplication.java
@@ -3,9 +3,11 @@ package com.nyist.microserviceconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ConsumerFeignMovieApplication {
 
 	public static void main(String[] args) {
